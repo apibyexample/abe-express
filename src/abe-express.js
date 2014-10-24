ba54459a4d2d73595db8e29002a94a0a41d2fafb
Chris Villa <chrisvilla@me.com>
2014-10-24 14:50:13 +0100

    Added more mock-check types as object

--- src/abe-express.js
+++ src/abe-express.js
@@ -1,27 +1,47 @@
 var request = require('supertest'),
     expect = require('expect.js'),
-    expressMockCheck = function (app, mock, example, done) {
-        var exampleMock = mock.examples[example],
-            method = mock.method.toLowerCase(),
-            mockBody = exampleMock.request.body;
+    expressMockCheck = {
+        full: function (app, mock, example, done) {
+            var exampleMock = mock.examples[example],
+                method = mock.method.toLowerCase(),
+                mockBody = exampleMock.request.body;
 
-        request(app)
-            [method](exampleMock.request.url)
-            .send(mockBody)
-            .end(function (err, res) {
-                var body = res.text,
-                    type = exampleMock.response.headers['Content-Type'];
+            request(app)
+                [method](exampleMock.request.url)
+                .send(mockBody)
+                .end(function (err, res) {
+                    var body = res.text,
+                        type = exampleMock.response.headers['Content-Type'];
 
-                if (type === 'application/json') {
-                    body = JSON.parse(res.text);
-                }
+                    if (type === 'application/json') {
+                        body = JSON.parse(res.text);
+                    }
 
-                expect(res.statusCode).to.be(exampleMock.response.status);
-                expect(res.headers['content-type']).to.contain(type);
-                expect(body).to.eql(exampleMock.response.body);
+                    expect(res.statusCode).to.be(exampleMock.response.status);
+                    expect(res.headers['content-type']).to.contain(type);
+                    expect(body).to.eql(exampleMock.response.body);
 
-                done();
-            });
+                    done();
+                });
+        },
+        header: function (app, mock, example, done) {
+            var exampleMock = mock.examples[example],
+                method = mock.method.toLowerCase(),
+                mockBody = exampleMock.request.body;
+
+            request(app)
+                [method](exampleMock.request.url)
+                .send(mockBody)
+                .end(function (err, res) {
+                    var body = res.text,
+                        type = exampleMock.response.headers['Content-Type'];
+
+                    expect(res.statusCode).to.be(exampleMock.response.status);
+                    expect(res.headers['content-type']).to.contain(type);
+
+                    done();
+                });
+        }
     };
 
 module.exports = expressMockCheck;
